963087fddfa44573910fb67fe705591a7ae3be55
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-19 19:36:02 +0200

    tailwind config file colors in extend added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import React, { Component } from "react";
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="text-4xl font-bold text-green-600">Hallo Welt!</h1>
+      <div className="bg-mainbg">
+        <h1 className="text-4xl text-white font-bold">Hallo Welt!</h1>
       </div>
     );
   }
---
+++ src/components/dash.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class dash extends Component {
+  render() {
+    return (
+      <div className='bg-mainbg'>
+        <span>Hello World</span>
+      </div>
+    )
+  }
+}
