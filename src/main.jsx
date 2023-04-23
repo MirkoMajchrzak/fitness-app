c48c52817e6025cebaea296dbed7b79b1c346c2f
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-23 11:20:35 +0200

    dashboard gebaut

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Dash from "./App/dash";
+import Nav from "./components/navbar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dash />
+    <Nav />
   </React.StrictMode>,
   document.getElementById("root")
 );
