6ac4ef80e84d5e4586889b3ed6e623f12d761b27
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-23 20:12:38 +0200

    main.jsx prettier

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,25 @@ import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
-  InMemoryCache
-} from '@apollo/client';
+  InMemoryCache,
+} from "@apollo/client";
 import "./index.css";
 import Dash from "./App/dash";
 import Nav from "./components/navbar";
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: "http://localhost:4000",
 });
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Dash />
     <Nav />
-    </ApolloProvider>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
