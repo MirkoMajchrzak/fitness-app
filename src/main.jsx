21b4add8f91398ba0f1d339a4512e829d20844f9
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-23 14:28:47 +0200

    weitere vorbereitungen

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
 import "./index.css";
 import Dash from "./App/dash";
 import Nav from "./components/navbar";
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <Dash />
     <Nav />
-  </React.StrictMode>,
+    </ApolloProvider>,
   document.getElementById("root")
 );
