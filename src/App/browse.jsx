0a9355299a3c11470b65c8ef2882a9a751e0fd8e
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-24 05:49:46 +0200

    prettier fixes

--- src/App/browse.jsx
+++ src/App/browse.jsx
@@ -4,12 +4,9 @@ export default function browse() {
   return (
     <div className="mx-5 flex-col justify-center">
       <h2 className="text-2xl">Browse</h2>
-      <div className="h-52 w-full bg-gradient-to-br from-orange to-pink rounded-3xl">
-      </div>
-      <div className="h-52 w-full bg-gradient-to-br from-cyan to-yellowgreen rounded-3xl">
-      </div>
-      <div className="h-52 w-full bg-gradient-to-br from-greenblue to-seablue rounded-3xl">
-      </div>
+      <div className="h-52 w-full bg-gradient-to-br from-orange to-pink rounded-3xl"></div>
+      <div className="h-52 w-full bg-gradient-to-br from-cyan to-yellowgreen rounded-3xl"></div>
+      <div className="h-52 w-full bg-gradient-to-br from-greenblue to-seablue rounded-3xl"></div>
     </div>
   );
 }
