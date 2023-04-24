65a7c0cf9130ad2fb79f1b5bbf208106bd75a65b
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-24 14:53:36 +0200

    exercices browse design finished

--- src/App/browse.jsx
+++ src/App/browse.jsx
@@ -2,11 +2,23 @@ import React from "react";
 
 export default function browse() {
   return (
-    <div className="mx-5 flex-col justify-center">
+    <div className="mx-5 flex-col justify-center space-y-4">
       <h2 className="text-2xl">Browse</h2>
-      <div className="h-52 w-full bg-gradient-to-br from-orange to-pink rounded-3xl"></div>
-      <div className="h-52 w-full bg-gradient-to-br from-cyan to-yellowgreen rounded-3xl"></div>
-      <div className="h-52 w-full bg-gradient-to-br from-greenblue to-seablue rounded-3xl"></div>
+      <div className="flex flex-col justify-center h-48 w-full bg-gradient-to-br from-orange to-pink rounded-3xl">
+        <h2 className="ml-12">Titel des Programms</h2>
+      </div>
+      <div className="flex flex-col justify-center h-48 w-full bg-gradient-to-br from-cyan to-yellowgreen rounded-3xl">
+      <h2 className="ml-12">100 Push-Ups Challenge</h2>
+      </div>
+      <div className="flex flex-col justify-center h-48 w-full bg-gradient-to-br from-greenblue to-seablue rounded-3xl">
+      <h2 className="ml-12">Titel des Programms</h2>
+      </div>
+      <div className="flex flex-col justify-center h-48 w-full bg-gradient-to-br from-orange to-pink rounded-3xl">
+        <h2 className="ml-12">Stretch and Relax</h2>
+      </div>
+      <div className="flex flex-col justify-center h-48 w-full bg-gradient-to-br from-cyan to-yellowgreen rounded-3xl">
+      <h2 className="ml-12">Titel des Programms</h2>
+      </div>
     </div>
   );
 }
--- src/App/dash.jsx
+++ src/App/dash.jsx
@@ -9,15 +9,15 @@ export default function dash() {
           <img className="w-10/12" src="./images/homescreen.svg" />
         </div>
         <div className="mt-10 flex justify-between items-end">
-          <h2 className="font-bold">Dein Workout heute</h2>
+          <h2>Dein Workout heute</h2>
           <p className="font-normal text-xs">Trainingsplan</p>
         </div>
         <div className="h-52 w-full bg-greybg rounded-3xl">
           <div className="h-full m-7 flex flex-col justify-end">
             <h3 className="text-lg font-bold leading-7">Tag 2</h3>
-            <h3 className="text-2xl font-bold leading-9">
+            <h2>
               Titel des Programms
-            </h3>
+            </h2>
             <p className="mb-7 text-xs">26 Min. · Beweglichkeit</p>
           </div>
         </div>
