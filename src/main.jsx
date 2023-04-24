eb25fe111112b5d7d9d704e65dd56f62cc998eb1
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-24 21:01:16 +0200

    prettier fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-} from "@apollo/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import "./index.css";
 import Dash from "./App/dash";
 import Browse from "./App/browse";
