abe63c8279c09e8873b708d10de744b7c8f84955
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-04-26 14:07:15 +0200

    nav

--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,28 @@
+import React from "react";
 import ReactDOM from "react-dom";
-import { useState } from "react";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import "./index.css";
 import Dash from "./App/dash";
-import Browse from "./App/browse";
-import Profile from "./App/profile";
-import Navbar from "./components/Navbar";
+import Nav from "./components/navbar";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
 
 const client = new ApolloClient({
-  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clge1u44d0e9f01upbd60ajac/master",
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
-const App = () => {
-  const [activeScreen, setActiveScreen] = useState("Dash");
-
-  const handleBrowseButtonClick = () => {
-    console.log("lead me to browse!");
-    setActiveScreen("Browse");
-  };
-
-  const handleHomeButtonClick = () => {
-    console.log("lead me to home!");
-    setActiveScreen("Dash");
-  };
-
-  const handleProfileButtonClick = () => {
-    console.log("lead me to my profile!");
-    setActiveScreen("Profile");
-  };
-
-  const renderScreen = () => {
-    switch (activeScreen) {
-      case "Dash":
-        return <Dash />;
-      case "Browse":
-        return <Browse />;
-      case "Profile":
-        return <Profile />;
-      default:
-        return <Dash />;
-    }
-  };
-
-  return (
-    <ApolloProvider client={client}>
-      <div>
-        <Navbar
-          onBrowseButtonClick={handleBrowseButtonClick}
-          onHomeButtonClick={handleHomeButtonClick}
-          onProfileButtonClick={handleProfileButtonClick}
-        />
-        {renderScreen()}
-      </div>
-    </ApolloProvider>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Dash />
+    <Nav />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
