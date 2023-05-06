0e2401f4251876544898c02cc8bccd8f3d04d517
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-05-06 12:26:40 +0200

    DefaultLayout

--- src/Layouts/DefaultLayout.jsx
+++ src/Layouts/DefaultLayout.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 export default function DefaultLayout({ className, children }) {
   return (
-    <div className={`bg-bgdark text-white px-6 py-14 ${className}`}>
+    <div className={`bg-bgdark text-white px-6 ${className}`}>
       {children}
       <Navbar />
     </div>
