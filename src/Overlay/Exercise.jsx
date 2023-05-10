e5e106444438c53478dc855bf561f617c773bad7
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-05-10 20:36:42 +0200

    try and error in exercise-popup

--- src/Overlay/Exercise.jsx
+++ src/Overlay/Exercise.jsx
@@ -6,24 +6,21 @@ function Exercise() {
       <button className="fixed top-5 right-5">
         <Close />
       </button>
-      <div className="top-0 h-3/4 w-full bg-gradient-to-br from-orange to-pink flex flex-col justify-center">
-        <div className="">
-          <h1 className="text-center">Excercise</h1>
-          <div className="flex justify-around">
-            <div className="flex flex-col justify-center items-center">
-              <div className="w-6 h-6 rounded-full bg-greybg"></div>
-              <p className="text-xs leading-3 uppercase text-center">Kraft</p>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <div className="w-6 h-6 rounded-full bg-greybg"></div>
-              <p className="text-xs leading-3 uppercase text-center">Leicht</p>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <div className="w-6 h-6 rounded-full bg-greybg"></div>
-              <p className="text-xs leading-3 uppercase text-center">
-                6 Wochen
-              </p>
-            </div>
+      <div className="inset-0 h-3/4 w-full bg-gradient-to-br from-orange to-pink flex flex-col justify-center">
+        <h1 className="flex flex-col justify-center text-center">Excercise</h1>
+
+        <div className="flex justify-around">
+          <div className="flex flex-col justify-center items-center">
+            <div className="w-6 h-6 rounded-full bg-greybg"></div>
+            <p className="text-xs leading-3 uppercase text-center">Kraft</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <div className="w-6 h-6 rounded-full bg-greybg"></div>
+            <p className="text-xs leading-3 uppercase text-center">Leicht</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <div className="w-6 h-6 rounded-full bg-greybg"></div>
+            <p className="text-xs leading-3 uppercase text-center">6 Wochen</p>
           </div>
         </div>
       </div>
