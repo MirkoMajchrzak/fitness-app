c967af2e48cd9ff3dd6c465f69522bc8168b73fc
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-05-12 23:08:26 +0200

    building exercise section from beginning, it seems to work now, design 80% finnished, i guess

--- src/App/Exercise.jsx
+++ src/App/Exercise.jsx
@@ -1,5 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
+import figpie, { ReactComponent as PieIcon } from "../images/figpie.svg";
 import close, { ReactComponent as Close } from "../images/close.svg";
 
 const PROGRAMS = gql`
@@ -19,30 +20,63 @@ export default function Exercise() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="h-[75vh] w-full bg-gradient-to-br from-orange to-pink">
-        <button onClick={() => navigate(-1)} className="fixed top-5 right-5">
-          <Close />
-        </button>
-        <div className="flex flex-col justify-between">
-          <h1 className="my-[44%] text-center">Titel des Programms</h1>
-          <div className="flex justify-around">
-            <div className="flex flex-col items-center">
-              <div className="h-6 w-6 bg-greybg rounded-full"></div>
-              <p className="mt-2 mb-4 uppercase text-center">Kraft</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <div className="h-6 w-6 bg-greybg rounded-full"></div>
-              <p className="mt-2 mb-4 uppercase text-center">Kraft</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <div className="h-6 w-6 bg-greybg rounded-full"></div>
-              <p className="mt-2 mb-4 uppercase text-center">Kraft</p>
-            </div>
+      <button onClick={() => navigate(-1)} className="fixed top-5 right-5">
+        <Close />
+      </button>
+      <div className="h-[75vh] bg-gradient-to-br from-orange to-pink flex flex-col justify-between">
+        <div className="pt-[25vh]">
+          <h1 className="text-center">Titel des Programms</h1>
+        </div>
+        <div className="flex justify-around">
+          <div className="flex flex-col items-center">
+            <div className="w-6 h-6 bg-greybg rounded-full"></div>
+            <p className="mb-4 mt-2 text-center uppercase">Kraft</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-6 h-6 bg-greybg rounded-full"></div>
+            <p className="mb-4 mt-2 text-center uppercase">Kraft</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-6 h-6 bg-greybg rounded-full"></div>
+            <p className="mb-4 mt-2 text-center uppercase">Kraft</p>
           </div>
         </div>
       </div>
-      <div className="mx-6 my-5">
-        <p>Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien. Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen.</p>
+      <div className="bg-greybg">
+        <p className="px-6 py-5">
+          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+          Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in
+          Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans.
+          Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt
+          sie mit den nötigen Regelialien. Es ist ein paradiesmatisches Land, in
+          dem einem gebratene Satzteile in den Mund fliegen.
+        </p>
+      </div>
+      <div className="px-6 mt-6">
+        <h3>So ist das Programm aufgeteilt:</h3>
+        <div className="flex items-center mt-10">
+          <div className="">
+            <PieIcon />
+          </div>
+          <div className="ml-8 flex flex-col gap-5">
+            <div className="flex">
+              <div className="bg-greybg h-3 w-3 rounded-full mr-3"></div>
+              <p className="text-xs">Krafttraining</p>
+            </div>
+            <div className="flex">
+              <div className="bg-greybg h-3 w-3 rounded-full mr-3"></div>
+              <p className="text-xs">Koordination</p>
+            </div>
+            <div className="flex">
+              <div className="bg-greybg h-3 w-3 rounded-full mr-3"></div>
+              <p className="text-xs">Cardio</p>
+            </div>
+            <div className="flex">
+              <div className="bg-greybg h-3 w-3 rounded-full mr-3"></div>
+              <p className="text-xs">Beweglichkeit</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
