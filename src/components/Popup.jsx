0e227003946420cbdf7d871e0e9ecaccb6963c9b
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-05-26 21:07:22 +0200

    link back from popup to the workout with the right id is now working, have to clean up the gql

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,43 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const PROGRAM = gql`
+  query Program($id: ID!) {
+    program(where: { id: $id }) {
+      id
+      name
+      description
+      focus
+      duration
+      difficulty
+      image {
+        url
+      }
+      workouts {
+        id
+        duration
+        category
+      }
+    }
+  }
+`;
 
 export default function Popup({ onClose }) {
+  const { id } = useParams();
+
+  const { data, loading, error } = useQuery(PROGRAM, {
+    variables: { id },
+  });
+  console.log(data);
+
+  if (loading) {
+    return <h2>Loading, take your supps... </h2>;
+  }
+  if (error) {
+    return <h2>Something went wrong...</h2>;
+  }
+
+  const { program } = data;
   return (
     <div className="absolute flex justify-center top-[50px] left-0 z-50">
       <div className="w-full h-[50%] bg-greybg rounded-3xl">
@@ -14,7 +51,7 @@ export default function Popup({ onClose }) {
           >
             <p>Nein, weiter machen!</p>
           </button>
-          <NavLink to="/browse">
+          <NavLink to= {`/exercise/${id}`}>
             <button className="hover:bg-mainbg rounded-3xl px-6 py-3">
               <p>Ja, beenden</p>
             </button>
