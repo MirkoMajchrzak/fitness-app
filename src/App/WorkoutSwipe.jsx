5ca99f163459e99f4f80f2430e31cb18a1ac8721
Mirko Majchrzak <mirkomajchrzak@hotmail.com>
2023-07-16 07:37:31 +0200

    fixed loading in swiper

--- src/App/WorkoutSwipe.jsx
+++ src/App/WorkoutSwipe.jsx
@@ -59,13 +59,7 @@ function WorkoutSwipe() {
   });
   const [activeSlide, setActiveSlide] = useState(0);
 
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center">
-        <img className="w-1/2" src="../images/loading.gif" alt="loading" />
-      </div>
-    );
-  }
+  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   console.log(data, workoutId);
 
